5ece1a24893429ace037249d81894b50618cdac9
andrees0801 <58889000+andrees0801@users.noreply.github.com>
2020-02-25 19:37:59 -0600

    inicio

---
+++ application/views/footer.php
@@ -0,0 +1,30 @@
+	</div>
+	<!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="<?php echo base_url(); ?>libs/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?php echo base_url(); ?>libs/bootstrap/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>libs/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="<?php echo base_url(); ?>libs/perfect-scrollbar.jquery.min.js"></script>
+    <!--Wave Effects -->
+    <script src="<?php echo base_url(); ?>libs/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="<?php echo base_url(); ?>libs/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="<?php echo base_url(); ?>libs/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="<?php echo base_url(); ?>libs/sparkline/jquery.sparkline.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="<?php echo base_url(); ?>libs/custom.min.js"></script>
+    <!-- Calendar JavaScript -->
+    <script src="<?php echo base_url(); ?>libs/calendar/jquery-ui.min.js"></script>
+    <script src="<?php echo base_url(); ?>libs/moment/moment.js"></script>
+    <script src='<?php echo base_url(); ?>libs/calendar/dist/fullcalendar.min.js'></script>
+    <script src="<?php echo base_url(); ?>libs/calendar/dist/cal-init.js"></script>
+    <!-- ============================================================== -->
+    <!-- Style switcher -->
+    <!-- ============================================================== -->
+    <script src="<?php echo base_url(); ?>libs/styleswitcher/jQuery.style.switcher.js"></script>
+</body>
+
+</html>
---
+++ application/views/nav-lateral.php
@@ -0,0 +1,17 @@
+		<aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        
+					<li>
+                    	<a class="waves-effect waves-dark transparente titulo-dash" href="#" aria-expanded="false"><i class="mdi mdi-home-outline"></i><span class="hide-menu">Home</span></a>
+                    </li>
+                        
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
