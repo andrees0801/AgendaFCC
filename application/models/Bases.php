3cd316ae425020e534182f5141dcda82af0f83e9
andrees0801 <58889000+andrees0801@users.noreply.github.com>
2020-04-01 23:16:06 -0600

    error solucionado, en el archivo databases.php la variable 'stricton' => TRUE

--- application/models/Bases.php
+++ application/models/Bases.php
@@ -6,14 +6,6 @@
 			parent::__construct();
 		}
 
-		// $query = $this->db->query($sql);
-		
-		// if($query->num_rows() > 0)
-		// {
-		// 	return $query->result();
-		// }else{
-		// 	return FALSE;
-		// }
 
 		public function agregar_solicitud($solicitud_id, $nombre, $apellido, $correo, $evento)
 		{
@@ -29,7 +21,7 @@
 
 		public function verificar_horario($dia, $inicio, $fin, $espacio)
 		{
-			$sql = "SELECT id_horario FROM horarios as h inner join solicitudes as s on h.solicitud_id = s.solicitud_id WHERE h.fecha LIKE '$dia' AND s.aprobado LIKE 'TRUE' AND s.verificado LIKE 'TRUE' AND h.espacio LIKE '$espacio'  AND ( ('$inicio' BETWEEN  h.hora_inicial AND h.hora_fin ) OR ( '$fin' BETWEEN  h.hora_inicial AND h.hora_fin ) OR ( TIMEDIFF(h.hora_fin, h.hora_inicial) < TIMEDIFF('$fin', '$inicio') )  )";
+			$sql = "SELECT h.id_horario FROM horarios as h inner join solicitudes as s on h.solicitud_id = s.solicitud_id WHERE h.fecha LIKE '$dia' AND s.aprobado LIKE 'TRUE' AND s.verificado LIKE 'TRUE' AND h.espacio LIKE '$espacio'  AND ( ('$inicio' BETWEEN  h.hora_inicial AND h.hora_fin ) OR ( '$fin' BETWEEN  h.hora_inicial AND h.hora_fin ) OR ( TIMEDIFF(h.hora_fin, h.hora_inicial) < TIMEDIFF('$fin', '$inicio') )  )";
 			$query = $this->db->query($sql);
 			if($query->num_rows() > 0)
 			{
